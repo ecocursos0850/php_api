ad94c2323e0113582f1b9ab93ad6cf6f36e67a2a
Lefundes <angelolefundes@yahoo.com.br>
2024-05-02 11:16:21 -0300

    Initial

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -16,29 +16,43 @@ class Email extends CI_Controller {
     }
 
     public function send() {
-        $destinatario = $this->input->post("destinatario"); 
-        $assunto = $this->input->post("assunto"); 
-        $mensagem = $this->input->post("mensagem");
+        // Obtenha o corpo da requisição POST enviado como JSON
+        $json_data = file_get_contents('php://input');
+    
+        // Decodifique os dados JSON para um array associativo
+        $data = json_decode($json_data, true);
+    
+        // Verifique se os dados foram decodificados corretamente
+        if ($data === null) {
+            // Se não foi possível decodificar os dados JSON, retorne um erro
+            http_response_code(400); // Bad Request
+            echo "Erro: os dados JSON estão malformados.";
+            return;
+        }
+    
+        // Extraia os dados do array associativo
+        $destinatario = $data["destinatario"]; 
+        $assunto = $data["assunto"]; 
+        $mensagem = $data["mensagem"];
+    
         // Inicialize a biblioteca MailerSend
         $mailerSend = new MailerSend(['api_key' => 'mlsn.9023e774d8fbc73e63223fdd4f9369d80f7b8463f0f5ef124aea005d81e7af6c']);
-        //$mailerSend = new MailerSend(['api_key' => 'mlsn.a1e2dfc2bf70ec097b48d7e0d88e11f92fc08691d6092896e161fc2ce631c4e6']);
+    
         // Configurar destinatários e parâmetros de e-mail
         $recipients = [
             new Recipient($destinatario, $destinatario),
         ];
-
+    
         $emailParams = (new EmailParams())
-            //->setFrom('MS_k7Zj7A@trial-pxkjn41xew0gz781.mlsender.net')
             ->setFrom('diretoria@ecocursos.com.br')
             ->setFromName('ECOCURSOS')
             ->setRecipients($recipients)
             ->setSubject($assunto)
             ->setHtml($mensagem)
             ->setText($mensagem);
-            //->setReplyTo('reply to')
-            //->setReplyToName('reply to name');
-
+    
         // Enviar e-mail usando a biblioteca MailerSend
         $mailerSend->email->send($emailParams);
     }
+    
 }
