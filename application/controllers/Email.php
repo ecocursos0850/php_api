61dd81f3cb20130fd03953f009493e2cd8f800ff
Lefundes <angelolefundes@yahoo.com.br>
2024-05-18 14:35:01 -0300

    Evolutivas-layout-envio-de-email

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -138,6 +138,68 @@ class Email extends CI_Controller {
         // Enviar e-mail usando a biblioteca MailerSend
         $mailerSend->email->send($emailParams);
     }
+
+    public function requestPostgraduate(){
+        // Obtenha o corpo da requisição POST enviado como JSON
+        $json_data = file_get_contents('php://input');
+    
+        // Decodifique os dados JSON para um array associativo
+        $data = json_decode($json_data, true);
+    
+        // Verifique se os dados foram decodificados corretamente
+        if ($data === null) {
+            // Se não foi possível decodificar os dados JSON, retorne um erro
+            http_response_code(400); // Bad Request
+            echo "Erro: os dados JSON estão malformados.";
+            return;
+        }
+    
+        // Dados para a view
+        $dados = [
+            'destinatario' => $data["destinatario"],
+            'assunto' => $data["assunto"],
+            'login' => $data["login"], // Adicione a variável login
+            'senha' => $data["senha"], // Adicione a variável senha
+            'copy' => date("Y") // Adicione a variável copy
+        ];
+
+        // Renderize a view e capture o conteúdo como string
+        try {
+            $mensagem_html = $this->twig->render('email/requestPostgraduate.twig', $dados);
+        } catch (Exception $e) {
+            log_message('error', 'Erro ao renderizar o template: ' . $e->getMessage());
+            show_error('Erro ao renderizar o template: ' . $e->getMessage(), 500);
+            return;
+        }
+
+        // Inicialize a biblioteca MailerSend
+        $api_key = getenv('MAILERSEND_API_KEY'); // Use a variável de ambiente para a API key
+        if (!$api_key) {
+            http_response_code(500); // Internal Server Error
+            echo "Erro: a chave da API não está configurada.";
+            return;
+        }
+
+
+        // Inicialize a biblioteca MailerSend
+        $mailerSend = new MailerSend(['api_key' => $api_key]); //oficial
+    
+        // Configurar destinatários e parâmetros de e-mail
+        $recipients = [
+            new Recipient($dados['destinatario'], $dados['destinatario']),
+        ];
+    
+        $emailParams = (new EmailParams())
+            ->setFrom('diretoria@ecocursos.com.br') //oficial
+            ->setFromName('ECOCURSOS')
+            ->setRecipients($recipients)
+            ->setSubject($dados["assunto"])
+            ->setHtml($mensagem_html);
+    
+        // Enviar e-mail usando a biblioteca MailerSend
+        $mailerSend->email->send($emailParams);        
+    }
+
     // Teste de envio de email
     public function resetPasswordTest() {
         
