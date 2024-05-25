37b2fca46f4b473ec3d81004ca229f4bceed15d1
Lefundes <angelolefundes@yahoo.com.br>
2024-05-25 19:48:38 -0300

    Novo com imagens

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -41,6 +41,7 @@ class Email extends CI_Controller {
             'assunto' => $data["assunto"],
             'nome' => $data["nome"], // Adicione a variável senha
             'senha' => $data["senha"], // Adicione a variável senha
+            'logo' => base_url() . "assets/imgs/logo.png",
             'copy' => date("Y") // Adicione a variável senha
         ];
 
@@ -101,6 +102,8 @@ class Email extends CI_Controller {
                     'assunto' => "🎁 " . $aluno->nome . ", Ecocursos quer te dar um presente!",
                     'nome' => $aluno->nome,
                     'presente' => "ECO-" . strtoupper($presente),
+                    'logo' => base_url() . "assets/imgs/logo.png",
+                    'presente' => base_url() . "assets/imgs/presente.png",
                     'copy' => date("Y")
                 ];
         
@@ -205,6 +208,7 @@ class Email extends CI_Controller {
             'assunto' => $data["assunto"],
             'login' => $data["login"], // Adicione a variável login
             'senha' => $data["senha"], // Adicione a variável senha
+            'logo' => base_url() . "assets/imgs/logo.png",
             'copy' => date("Y") // Adicione a variável copy
         ];
 
